a7a6db61a6a0d9b6997add3eb7e61a4dd3dac8df
Nagasai Krishna <nagasaikrishna2000@gmail.com>
2023-12-04 23:35:14 -0500

    Final Project

--- assignments/project/project.py
+++ assignments/project/project.py
@@ -1,14 +1,38 @@
 import pandas as pd
 import time
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 
-class my_model():
+
+def clean(X):
+    X['description'] = X['description'].str.lower()
+    X['description'] = X['description'].str.replace('[^a-zA-Z]', ' ')
+    X['description'] = X['description'].str.replace('!', '')
+    return X
+
+
+class my_model:
+    def __init__(self):
+        self.preprocessor = CountVectorizer(stop_words='english', max_df=.7)
+        self.clf=RandomForestClassifier(n_estimators=100, random_state=42)
+
     def fit(self, X, y):
         # do not exceed 29 mins
+        X=clean(X)
+        X=X['description']
+        XX = self.preprocessor.fit_transform(X)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).fit_transform(XX)
+        self.clf.fit(X_final, y)
         return
 
     def predict(self, X):
-        # remember to apply the same preprocessing in fit() on test data before making predictions
-        return predictions
-
+        X=clean(X)
+        X=X['description']
+        XX = self.preprocessor.transform(X)
+        X_final = TfidfTransformer(norm='l2', use_idf=False, smooth_idf=False, sublinear_tf=True).fit_transform(XX)
+        predictionsOfModel = self.clf.predict(X_final)
+        return predictionsOfModel
 
 
